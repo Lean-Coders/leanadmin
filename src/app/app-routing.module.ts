ded8171c15f35728383b109570dd91bc8a8c9917
Manu <manuela.abdalla@lean-coders.com>
2018-09-03 13:19:00 +0200

    forgotpw screen design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./auth/login/login.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ForgotComponent } from "./auth/forgot/forgot.component";
 
 const routes: Routes = [
     { path: '', component: LoginComponent},
+    { path: 'forgot', component: ForgotComponent},
     { path: 'dashboard', component: DashboardComponent},
 ];
 
