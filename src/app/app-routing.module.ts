d41db36ce0b0d8ceec3f8e3bf7d9d3464b74d2f7
Manu <manuela.abdalla@lean-coders.com>
2018-09-14 12:24:55 +0200

    redirekt directly to dashboard after logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "./auth/login/login.component";
-import { AdminboardComponent } from "./adminboard/adminboard.component";
 import { ForgotComponent } from "./auth/forgot/forgot.component";
 import { ReservationComponent } from "./adminboard/reservation/reservation.component";
 import { AuthGuard } from "./auth/login/auth.guard";
 import { DashboardComponent } from "./adminboard/dashboard/dashboard.component";
 
 const routes: Routes = [
-    {
-        path: '',
-        component: AdminboardComponent,
-        canActivate: [AuthGuard]
-    },
     {
         path: 'dashboard',
         component: DashboardComponent,
