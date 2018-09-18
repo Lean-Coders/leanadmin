631f45f34b8a8b06083dd8fb560210d808ba2483
Manu <manuela.abdalla@lean-coders.com>
2018-09-18 15:13:39 +0200

    added responsive table

--- src/app/adminboard/reservation/reservation.component.ts
+++ src/app/adminboard/reservation/reservation.component.ts
@@ -1,6 +1,30 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from "@angular/material";
 
+@Component({
+  selector: 'app-reservation',
+  templateUrl: './reservation.component.html',
+  styleUrls: ['./reservation.component.scss']
+})
+export class ReservationComponent implements OnInit{
+
+    displayedColumns = ['date', 'name', 'email', 'person', 'type', 'state', 'actionsColumn'];
+    dataSource = new MatTableDataSource<ReservationDetails>(ELEMENT_DATA);
+
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
+
+
+    ngOnInit() {
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+    }
+
+    doFilter(filterValue: string) {
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
+}
+
 export interface ReservationDetails {
     date: string;
     name: string;
@@ -17,29 +41,6 @@ const ELEMENT_DATA: ReservationDetails[] = [
     {date: '06.08', name: 'Test Daten', email: 'test@daten.com', person: '2', type: 'Normal', state: 'storniert'},
     {date: '06.08', name: 'Test Daten', email: 'test@daten.com', person: '10', type: 'Geburtstag', state: 'abgeschlossen'},
     {date: '06.08', name: 'Test Daten', email: 'test@daten.com', person: '3', type: 'Normal', state: 'storniert'}
-    ];
-
-@Component({
-  selector: 'app-reservation',
-  templateUrl: './reservation.component.html',
-  styleUrls: ['./reservation.component.scss']
-})
-export class ReservationComponent implements OnInit{
-
-    displayedColumns: string[] = ['date', 'name', 'email', 'person', 'type', 'state', 'actionsColumn'];
-    dataSource = new MatTableDataSource<ReservationDetails>(ELEMENT_DATA);
-
-    @ViewChild(MatPaginator) paginator: MatPaginator;
-    @ViewChild(MatSort) sort: MatSort;
+];
 
 
-    ngOnInit() {
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-    }
-
-    doFilter(filterValue: string) {
-        this.dataSource.filter = filterValue.trim().toLowerCase();
-    }
-
-}
